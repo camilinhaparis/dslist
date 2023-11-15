b966f6e6a129d15d3b97b2e8f72448694e77f545
Camila Parise <camilinhaparis@hotmail.com>
2023-11-16 00:41:35 +0100

    Projection,query

--- src/main/java/com/camilaparise/dslist/services/GameService.java
+++ src/main/java/com/camilaparise/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.camilaparise.dslist.dto.GameDTO;
 import com.camilaparise.dslist.dto.GameMinDTO;
 import com.camilaparise.dslist.entities.Game;
+import com.camilaparise.dslist.projections.GameMinProjection;
 import com.camilaparise.dslist.repositories.GameRepository;
 
 
@@ -29,6 +30,14 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result =  gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result =  gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
 
